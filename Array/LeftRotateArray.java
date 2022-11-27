44adc77d03638932abfbe4d3172f61f99bc492b7
vishwa <karkavishwanath230@gmail.com>
2022-11-27 10:49:08 +0530

    edited leftRoteArray

--- Array/LeftRotateArray.java
+++ Array/LeftRotateArray.java
@@ -1,10 +1,10 @@
 public class LeftRotateArray {
-    static void leftRotation(int arr[], int n, int d){
-        reverse(arr,0,d-1);
-        reverse(arr,d,n-1);
-        reverse(arr,0,n-1);
+    static void leftRotation(int arr[], int n, int d){// d =3
+        reverse(arr,0,d-1); // [3,2,1]
+        reverse(arr,d,n-1); // [9 8 7 6 5 4 ]
+        reverse(arr,0,n-1);// [4 5 6 7 8 9 1 2 3 ]
     }
-
+//[]
     static void reverse(int arr[], int low,int high){
         while(low<high){
             int temp = arr[low];
@@ -15,30 +15,7 @@ public class LeftRotateArray {
         }
     }
 //    Method -2
-//    static void leftRotate(int arr[], int d, int n)
-//    {
-//        reverse(arr, 0, d - 1);
-//
-//        reverse(arr, d, n - 1);
-//
-//        reverse(arr, 0, n - 1);
-//
-//    }
-//
-//    static void reverse(int arr[], int low, int high)
-//    {
-//        while(low < high)
-//        {
-//            int temp = arr[low];
-//            arr[low] = arr[high];
-//            arr[high] = temp;
-//
-//            low++;
-//            high--;
-//        }
-//    }
 
-//    Method -3
 static void leftRotate(int arr[], int d, int n)
 {
     int temp[] = new int[d];
@@ -57,7 +34,29 @@ static void leftRotate(int arr[], int d, int n)
     {
         arr[n - d + i] = temp[i];
     }
+
 }
+    // Method-3
+    static void lRotateOne(int arr[], int n)
+    {
+        int temp = arr[0];
+
+        for(int i = 1; i < n; i++) // shift one element to last [2,3,4,5,6,7,8,9,1]
+        {                         // Second  call [3,4,5,6,7,8,9,1,2]
+            arr[i - 1] = arr[i];
+        }
+
+        arr[n - 1] = temp;
+    }
+
+    static void leftRotate1(int arr[], int d, int n)
+    {
+        for(int i = 0; i < d; i++)
+        {
+            lRotateOne(arr, n);
+        }
+    }
+
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9};
