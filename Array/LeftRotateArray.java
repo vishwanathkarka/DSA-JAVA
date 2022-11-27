03dcb56abcba3de408d7c691ec3605a2b0bd5cc8
vishwa <karkavishwanath230@gmail.com>
2022-11-27 10:37:55 +0530

    added more method for solving LeftRotateArry

--- Array/LeftRotateArray.java
+++ Array/LeftRotateArray.java
@@ -14,6 +14,51 @@ public class LeftRotateArray {
             high--;
         }
     }
+//    Method -2
+//    static void leftRotate(int arr[], int d, int n)
+//    {
+//        reverse(arr, 0, d - 1);
+//
+//        reverse(arr, d, n - 1);
+//
+//        reverse(arr, 0, n - 1);
+//
+//    }
+//
+//    static void reverse(int arr[], int low, int high)
+//    {
+//        while(low < high)
+//        {
+//            int temp = arr[low];
+//            arr[low] = arr[high];
+//            arr[high] = temp;
+//
+//            low++;
+//            high--;
+//        }
+//    }
+
+//    Method -3
+static void leftRotate(int arr[], int d, int n)
+{
+    int temp[] = new int[d];
+
+    for(int i = 0; i  < d; i++)
+    {
+        temp[i] = arr[i];
+    }
+
+    for(int i = d; i  < n; i++)
+    {
+        arr[i - d] = arr[i];
+    }
+
+    for(int i = 0; i  < d; i++)
+    {
+        arr[n - d + i] = temp[i];
+    }
+}
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9};
         leftRotation(arr,9,3);
