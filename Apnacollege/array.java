dbb0151fdf3c39841543dac918cbacffc452ee86
Vishwanthkarka <vishwanathreddykarka@gmail.com>
2023-04-30 11:01:27 +0530

    worked on the array apna college

--- Apnacollege/array.java
+++ Apnacollege/array.java
@@ -87,9 +87,109 @@ public static void  maxSubArray(int numbers[]){
     }
     System.out.println(maxSum);
 }
+
+
+
+public static void  maxSum(int numbers[]){
+
+    int postfix[] = new int[numbers.length];
+postfix[0]= numbers[0];
+    for(int i =1; i<numbers.length;i++){
+        postfix[i] = postfix[i-1]+numbers[i];
+    }
+    int currSum =0;
+    int maxVal = Integer.MIN_VALUE;
+    for(int i =0; i<numbers.length;i++){
+        for(int j =i ; j<numbers.length;j++){
+            if(i ==0){
+currSum = postfix[0];
+            }
+            else{
+          currSum =  postfix[j]-postfix[i-1];
+            }
+        }
+        if(maxVal < currSum){
+            maxVal = currSum;
+        }
+    }
+    System.out.println(maxVal);
+}
+
+public static void maxSum3(int numbers[]){
+    int maxSum = Integer.MIN_VALUE;
+    int currSum =0;
+    for(int i =0; i<numbers.length;i++){
+        currSum = currSum + numbers[i];
+        if(currSum <0){
+            currSum =0;
+        }
+        maxSum = Math.max(currSum,maxSum);
+    }
+    System.out.println(maxSum);
+
+}
+
+public static Boolean findNumber(int numbers[]){
+
+for(int i =0; i<numbers.length;i++){
+    for(int j =i+1;j<numbers.length;j++){
+if(numbers[i]== numbers[j]){
+    return true;
+}
+    }
+}
+return false;
+
+}
+
+public static int findIndexValue(int numbers[],int target){
+    for(int i =0; i<numbers.length;i++){
+        if(numbers[i]== target){
+            return i;
+        }
+    }
+    return -1;
+}
+public static int stockSellAndBuy(int numbers[]){
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+    for(int i =0; i<numbers.length;i++){
+if(minPrice>numbers[i]){
+    minPrice = numbers[i];
+}
+else{
+    int profit =  numbers[i]-minPrice;
+    maxProfit = Math.max(maxProfit,profit);
+}
+
+    }
+    return maxProfit;
+}
+
+public static int trappedRainWater(int height[]){
+    int totalWaterTrapped = 0;
+    int n = height.length;
+    int leftMax[] = new int[n];
+    int rightMax[] = new int[n];
+    leftMax[0] = height[0];
+    for(int i =1; i<n;i++){
+    leftMax[i] = Math.max(leftMax[i-1], height[i]);
+    }
+rightMax[n-1] = height[n-1];
+    for(int i =n-2; i>=0;i--){
+    rightMax[i] = Math.max(rightMax[i+1], height[i]);
+    }
+    for(int i =0;i<n;i++){
+int waterLevel = Math.min(leftMax[i],rightMax[i]);
+totalWaterTrapped += waterLevel-height[i];
+
+    }
+    return totalWaterTrapped;
+}
     public static void main(String args[]){
-    int arr[] = {1,2,3,4,5,6,7};
+    int arr[] = {7,1,5,3,6,4};
+    int arr1[] = {0,1,0,2,1,0,1,3,2,1,2,1};
     // pairArray(arr);
-    maxSubArray(arr);
+   System.out.println( trappedRainWater(arr1));
     }
 }
