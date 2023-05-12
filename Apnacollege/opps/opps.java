0abe2337f1af7d962d3bbf01762a29bff53755e3
Vishwanthkarka <vishwanathreddykarka@gmail.com>
2023-05-12 23:13:55 +0530

    worked on basic recurssion

--- Apnacollege/opps/opps.java
+++ Apnacollege/opps/opps.java
@@ -1,16 +1,99 @@
 package Apnacollege.opps;
 
 public class opps {
+
     public static void main(String args[]){
-Pen  p1 = new Pen();
+
+Student  p1 = new Student(10); // object creation
+Student  p2 = new Student("vishwa"); // object with sting as the parameters
+Student  p3 = new Student();// object with no parameters
+p3.initial = 0; // assign with value with in the class 
+p3.res = 56; 
+p3.marks[0] = 40;  //assigning the array value
+p3.marks[1] = 50;
+p3.marks[2]= 50;
+
+//Shallow copy contractor
+Student p4 = new Student(p3);  //passing the object in the another object
+
+
+// Shallow  -> This refers to the array
+// Deep copy -> This makes new array
+
+
+// changing the array value after the copy constructor
+p3.marks[0] = 80;  //assigning the array value
+p3.marks[1] = 90;
+p3.marks[2]= 100; // THIS WILL EFFECT THE VALUES IN THE COPY CONSTRUCTOR
+
 System.out.println(p1.res);
 
+p1.changePassword("hello world");
+p1.sum();
     }
 }
 
-  class Pen{
-int initial = 0; // default
-public int res = 10; // public
+  class Student{
+int initial ; // default
+public int res ; // public
+public String name ;
 private String password = "$&%&*#@((#$))"; // private
+int marks[]; //array
+
+// contructor with no parameters
+Student(){
+  System.out.println("No parameters");
+}
+
+// shallow copy contructor
+
+// Student(Student s1){
+//   marks = new int[3];
+// this.initial = s1.initial;
+// this.res = s1.res;
+// this.marks = s1.marks ; // this refers to the same array of the object
+// }
+
+
+// Deep copy constructor  making the new marks array  instead of the referring to the same array
+Student (Student s1){
+  marks = new int[3];
+this.initial = s1.initial;
+this.res = s1.res;
+for(int i =0; i<marks.length;i++){
+  this.marks[i] = s1.marks[i];
+}
+}
+
+//constructor with Sting parameter
+Student(String nameUser){
+  marks = new int[3];
+  this.name = nameUser;
+  System.out.println(this.name);
+}
+
+// constructors with int parameters
+Student(int val){
+  marks = new int[3];
+  this.initial = val;
+  System.out.println("this is constructor"+initial);
+}
+
+//setters
+public void changePassword (String ps){
+  System.out.println(this.password);
+  this.password = ps ;
+  System.out.println(this.password);
+}
+
+//getters
+public String getPassword(){
+  return this.password;
+}
+
+public void sum (){
+System.out.println(9+6);
+}
+
 
 }
---
+++ Apnacollege/recurssion.java
@@ -0,0 +1,139 @@
+package Apnacollege;
+
+public class recurssion {
+     static void  print (int n){
+
+if(n == 1){
+    return ;
+}
+// System.out.println(n);
+//10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+ print(n-1);
+ System.out.println(n);
+//  2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+    }
+
+    static int fib(int n){
+        if(n== 1 || n==0){
+return 1;
+        }
+      return  fib(n-1)+fib(n-2);
+    //   8%  
+
+    }
+    
+    static int fact (int n){
+        if(n == 1){
+            return 1;
+        }
+    //   return n* fact(n-1);
+    int f = fact(n-1);
+    return n*f;
+    }
+    
+    static int sumNaturalNo(int n){
+        if(n==1){
+            return 1;
+        }
+    
+     int k  = sumNaturalNo(n-1);
+  int sn =   k+n;
+  return sn;
+    // return  n+ sumNaturalNo(n-1); 
+    }
+
+    static Boolean isSortedArray(int[] arr, int k){
+      if(arr[k]>arr[k+1]){
+return false;
+      }
+      if(k == arr.length-2){
+        return true;
+      }
+        
+      return  isSortedArray(arr,k+1);
+       
+      
+    }
+  
+    static int firstOcc(int[] arr,int k,int tar){
+    if(k == arr.length-1){
+        return -1;
+    }
+  
+
+  if(arr[k] == tar)
+    return k;
+return firstOcc(arr,k+1,tar);
+  
+  }
+  
+  static int lastOcc(int[] arr,int k ,int tar){
+ if(k == arr.length){
+    return -1;
+ }
+ int res = lastOcc(arr, k+1,tar); //res will return the values
+ System.out.println(res);
+ System.out.println(k);
+ if(res == -1 && arr[k] == tar){
+    return k;
+ }
+ return res;
+  }
+
+  static int power(int x , int n){
+if(n== 0){
+    return 1;
+}
+// int val = power(x,n-1);
+// int pow = val *n;
+// return pow;
+return n* power(x,n-1);
+  }
+
+  // given 2*n space to place tailes , we need to find different ways to to place the tailes
+  // as the row are same 2 colums will change
+  static int tailing(int n){
+if(n == 0 || n == 1){
+  return 1;
+}
+// vertical choice => |
+int fvno = tailing(n-1);
+
+// Horizontal choice +> -
+int fhno = tailing(n-2);
+
+int toWays = fvno+fhno;
+return toWays;
+
+  }
+
+  public static void main(String[] args) {
+    //   print(10);
+    // System.out.print(fib(5));
+// System.out.print(fact(5));
+// 120
+
+// System.out.println(sumNaturalNo(5));
+// 15
+int arr[]= {1,33,33,33,55};
+// System.out.println(isSortedArray(arr,0));
+// System.out.println(power(2,10));
+System.out.println(tailing(5));
+}
+}
